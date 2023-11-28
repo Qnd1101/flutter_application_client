1906d29cde1eb0448bd56a7ab85132fcb0b1ba6d
MinJun <mj061101k@naver.com>
2023-11-28 18:06:10 +0900

    결제화면 만들기

--- lib/oreder_result.dart
+++ lib/oreder_result.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/foundation.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+String orderCollectionName = 'cafe-order';
+var firestore = FirebaseFirestore.instance;
 
 // 주문저장, 주문번호, 시간이 지나면 다시 메인으로 이동
 class OrderResult extends StatefulWidget {
@@ -14,11 +16,44 @@ class OrderResult extends StatefulWidget {
 class _OrderResultState extends State<OrderResult> {
   late Map<String, dynamic> orderResult;
 
+  Future<int> getOrderNumber() async {
+    //가장 마지막 번호
+    int number = 1;
+    var now = DateTime.now(); // 2023 -- 11-28 / 00 : 00 : 00
+    var s = DateTime(now.year, now.month, now.day); // 오늘의 00 : 00 : 00
+    // firebase의 시간은 타임스탬프값 1970.01.01
+
+    var today = Timestamp.fromDate(s);
+    try {
+      await firestore
+          .collection(orderCollectionName)
+          .where('orderTime', isGreaterThan: today)
+          .orderBy('orderTime', descending: true)
+          .limit(1)
+          .get()
+          .then((value) {
+        // value는 마지막 결과 하나
+        var data = value.docs;
+        number = data[0]['orderNumber'] + 1;
+      });
+    } catch (e) {
+      number = 1;
+    }
+    return number;
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    // 결과 준비
     orderResult = widget.orderResult;
+
+    // 현재 주문번호를 설정
+    getOrderNumber();
+    // 오늘을 기준으로 여태까지 개수 10건 -> 11번, 만약 한 건도 없으면 1번
+
+    //주문번호, 시간포함, 데이터 저장
   }
 
   @override
